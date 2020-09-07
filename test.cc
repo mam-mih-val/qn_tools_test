7cab371bcd255d1a9bca8baee1d1e8913a46352a
mam-mih-val <mam.mih.val@gmail.com>
2020-09-07 14:26:40 +0300

    Example of bug with Qn::DataContainer::Rebin()

--- test.cc
+++ test.cc
@@ -7,41 +7,67 @@ int main(int n_args, char **args) {
   auto file =
       TFile::Open(file_name.data());
   assert(file);
+
   // u1Q1 correlation
-  // Axis: {"Centrality", 8, 0, 40}, {"mdc_vtx_tracks_pT", 16, 0, 1.6}, {"y_cm", 15, -0.75, 0.75}
-  Qn::DataContainerStats *container_un_qn;
+  // Axes: {"Centrality", 8, 0, 40}, {"mdc_vtx_tracks_pT", 16, 0, 1.6}, {"y_cm", 15, -0.75, 0.75}
+  Qn::DataContainerStats *container_un_Qa;
   // Correlations needed for resolution calculation
   // All are single-axis {"Centrality", 8, 0, 40}
-  Qn::DataContainerStats *container_w1_w2;
-  Qn::DataContainerStats *container_w2_w3;
-  Qn::DataContainerStats *container_w1_w3;
+  Qn::DataContainerStats *container_Qa_Qb;
+  Qn::DataContainerStats *container_Qb_Qc;
+  Qn::DataContainerStats *container_Qa_Qc;
 
-  file->GetObject("u_RESCALED_W1_RECENTERED_Q1x_Q1x", container_un_qn);
-  file->GetObject("W1_RECENTERED_W2_RECENTERED_Q1x_Q1x", container_w1_w2);
-  file->GetObject("W1_RECENTERED_W3_RECENTERED_Q1x_Q1x", container_w1_w3);
-  file->GetObject("W2_RECENTERED_W3_RECENTERED_Q1x_Q1x", container_w2_w3);
+  // reading containers from file
+  file->GetObject("u_RESCALED_W1_RECENTERED_Q1x_Q1x",
+                  container_un_Qa); // Observable, 3-axes: {"Centrality", 8, 0, 40}, {"mdc_vtx_tracks_pT", 16, 0, 1.6}, {"y_cm", 15, -0.75, 0.75}
+  file->GetObject("W1_RECENTERED_W2_RECENTERED_Q1x_Q1x",
+      container_Qa_Qb); // Reference, single-axis {"Centrality", 8, 0, 40}
+  file->GetObject("W1_RECENTERED_W3_RECENTERED_Q1x_Q1x",
+      container_Qa_Qc); // Reference, single-axis {"Centrality", 8, 0, 40}
+  file->GetObject("W2_RECENTERED_W3_RECENTERED_Q1x_Q1x",
+      container_Qb_Qc); // Reference, single-axis {"Centrality", 8, 0, 40}
 
-  auto resolution = Sqrt(*container_w1_w2 * *container_w1_w3 / *container_w2_w3); // Simple 3-Sub method
-  auto flow_3d = *container_un_qn / resolution; // Impossible to make any projection or rebin even if bins are just cut off
+  // Resolution calculation with 3-Sub method
+  // All containers in this operation are reference, Sqrt() is unary => result resolution is mergeable
+  auto resolution = Sqrt(*container_Qa_Qb * *container_Qa_Qc / *container_Qb_Qc);
+  // Flow calculation
+  // Observable / Reference => result flow is mergeable
+  auto flow = *container_un_Qa / resolution;
+
+  // Appearance 1: Rebin of mergeable container falls with error
+  Qn::DataContainerStats flow_rebinned;
   try{
-    flow_3d = flow_3d.Rebin({"Centrality", 1, 20, 25});
-    flow_3d = flow_3d.Rebin({"mdc_vtx_tracks_pT", 1, 1.0, 1.1});
-    flow_3d = flow_3d.Projection({"y_cm"});
+    std::cout << "Trying to rebin flow container centrality axis, halve bins in same range" << std::endl;
+    flow_rebinned = flow.Rebin({"Centrality", 4, 0, 40});
+    std::cout << "Rebin attempt was successful for flow container" << std::endl;
   } catch (const std::exception& e) {
-    std::cout << "main() 27-28 lines catched: " << e.what() << std::endl;
+    std::cout << "Rebin attempt was unsuccessful for flow container: " << e.what() << std::endl;
   }
 
-  // Only centrality projection of flow is clickable, how to look at the differential flow?
-  auto un_qn_centrality = container_un_qn->Rebin({"y_cm", 1, -0.25, -0.15});
-  un_qn_centrality = un_qn_centrality.Projection({"Centrality"});
-  auto flow_1d = un_qn_centrality / resolution;
+  // Appearance 2: Rebin of mergeable container seems to be success, but with incorrect result
+  Qn::DataContainerStats resolution_rebinned;
+  try{
+    std::cout << "Trying to rebin resolution container centrality axis, halve bins in same range" << std::endl;
+    resolution_rebinned = resolution.Rebin({"Centrality", 4, 0.0, 40.0});
+    std::cout << "Rebin attempt was successful for resolution container" << std::endl;
+  } catch (const std::exception& e) {
+    std::cout << "Rebin attempt was unsuccessful for resolution container: " << e.what() << std::endl;
+  }
 
   auto file_out = TFile::Open("result.root", "recreate");
   file_out->cd();
 
   resolution.Write("resolution");
-  flow_3d.Write("flow_all_axis");
-  flow_1d.Write("flow_centrality");
+  resolution_rebinned.Write("resolution_rebinned");
+  flow.Write("flow");
+  flow_rebinned.Write("flow_rebinned");
+
+  // original containers used for flow calculation
+  container_Qa_Qb->Write("<Qa,Qb>");
+  container_Qa_Qc->Write("<Qa,Qc>");
+  container_Qb_Qc->Write("<Qb,Qc>");
+  container_un_Qa->Write("<un,Qa>");
+
   file_out->Close();
   return 0;
 }
