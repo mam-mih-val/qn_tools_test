e3667982abfb2edb4942f92b4e30009a156ce8eb
mam-mih-val <mam.mih.val@gmail.com>
2021-03-12 15:24:54 +0300

    test on selection of bins

--- test.cc
+++ test.cc
@@ -1,5 +1,6 @@
 #include <DataContainer.hpp>
 #include <TFile.h>
+#include <TCanvas.h>
 
 int main(int n_args, char **args) {
   assert(n_args==2);
@@ -8,51 +9,40 @@ int main(int n_args, char **args) {
       TFile::Open(file_name.data());
   assert(file);
 
-  // u1Q1 correlation
-  // Axes: {"Centrality", 8, 0, 40}, {"mdc_vtx_tracks_pT", 16, 0, 1.6}, {"y_cm", 15, -0.75, 0.75}
-  Qn::DataContainerStatCollect *container_un_Qa;
-  // Correlations needed for resolution calculation
-  // All are single-axis {"Centrality", 8, 0, 40}
-  Qn::DataContainerStatCollect *container_Qa_Qb;
-  Qn::DataContainerStatCollect *container_Qb_Qc;
-  Qn::DataContainerStatCollect *container_Qa_Qc;
+  Qn::DataContainerStatCollect *container;
 
-  // reading containers from file
-  file->GetObject("/uQ/SP/u_RESCALED.W1_RESCALED.x1x1",
-                  container_un_Qa); // Observable, 3-axes: {"Centrality", 8, 0, 40}, {"mdc_vtx_tracks_pT", 16, 0, 1.6}, {"y_cm", 15, -0.75, 0.75}
-  file->GetObject("/QQ/SP/W2_RESCALED.W1_RESCALED.x1x1",
-      container_Qa_Qb); // Reference, single-axis {"Centrality", 8, 0, 40}
-  file->GetObject("/QQ/SP/W3_RESCALED.W1_RESCALED.x1x1",
-      container_Qa_Qc); // Reference, single-axis {"Centrality", 8, 0, 40}
-  file->GetObject("/QQ/SP/W3_RESCALED.W2_RESCALED.x1x1",
-      container_Qb_Qc); // Reference, single-axis {"Centrality", 8, 0, 40}
+  // Reading the object from the file:
+  // 3 axes: centrality (12, 0, 60); y (15, -0.75, 0.75); pT(10, 0.29375, 0.35625, 0.41875, 0.48125, 0.54375, 0.61875, 0.70625, 0.81875, 1.01875, 2) -- Non-uniform binning
+  file->GetObject( "uQ/reco/pid_protons_RESCALED.psi_rp_PLAIN.x1x1", container );
 
-  // Copying stats containers to stat bins
-  Qn::DataContainer<Qn::StatCalculate> stat_bins_un_Qa( *container_un_Qa );
-  Qn::DataContainer<Qn::StatCalculate> stat_bins_Qa_Qb( *container_Qa_Qb );
-  Qn::DataContainer<Qn::StatCalculate> stat_bins_Qa_Qc( *container_Qa_Qc );
-  Qn::DataContainer<Qn::StatCalculate> stat_bins_Qb_Qc( *container_Qb_Qc );
-
-  // Resolution calculation with 3-Sub method
-  auto resolution = Sqrt(stat_bins_Qa_Qb * stat_bins_Qa_Qc / stat_bins_Qb_Qc)*sqrt(2.0);
-  // Flow calculation
-  auto flow = stat_bins_un_Qa*2.0 / resolution;
-
+  auto v1_pT = container->Select( {"event_header_selected_tof_rpc_hits_centrality", 2, 20, 30} ); // Selection 2 bins in centrality axis
+  v1_pT = v1_pT.Select({"mdc_vtx_tracks_rapidity_rapidity", 1, -0.25+0.74, -0.15+0.74}); // Selection of 1 bin on rapidity-axis
+  v1_pT = v1_pT.Projection( {"mdc_vtx_tracks_rapidity_pT"} ); // projection to pT-axis
+  v1_pT.Print();
+  auto v1_pT_selected = v1_pT.Select({"mdc_vtx_tracks_rapidity_pT", 9, 0.29375, 2.0}); // Selection of 9 bins on pT axis
+  v1_pT_selected.Print(); // Bin-edges changed
+  try {
+    auto v1_pT_rebinned = v1_pT.Rebin({"mdc_vtx_tracks_rapidity_pT", 9, 0.29375, 2.0});
+    v1_pT_rebinned.Print();
+  } catch (std::exception& e) {
+    std::cout << "Rebin was not successed: " << e.what() << std::endl;
+  }
+  auto canv = new TCanvas("canv", "");
+  canv->cd();
+  auto v1_pT_graph = Qn::ToTGraph( v1_pT );
+  v1_pT_graph->SetTitle( "All bins" );
+  v1_pT_graph->SetLineColor( kRed );
+  v1_pT_graph->Draw();
+  auto v1_pT_selected_graph = Qn::ToTGraph( v1_pT_selected );
+  v1_pT_selected_graph->SetLineColor( kBlue );
+  v1_pT_selected_graph->SetTitle("Selected 9 bins");
+  v1_pT_selected_graph->Draw("same");
+  gPad->BuildLegend();
   auto file_out = TFile::Open("result.root", "recreate");
-
+  canv->Write();
+  v1_pT.Write("oringinal_bins");
+  v1_pT_selected.Write("selected_bins");
   file_out->cd();
-  resolution.Write("resolution_bootstrap");
-  resolution.SetErrors(Qn::StatCalculate::ErrorType::PROPAGATION);
-  resolution.Write("resolution_propagation");
-  flow.Write("flow_bootstrap");
-  flow.SetErrors(Qn::StatCalculate::ErrorType::PROPAGATION);
-  flow.Write("flow_propagation");
-
-  // original containers used for flow calculation
-  stat_bins_Qa_Qb.Write("<Qa,Qb>");
-  stat_bins_Qa_Qc.Write("<Qa,Qc>");
-  stat_bins_Qb_Qc.Write("<Qb,Qc>");
-  stat_bins_un_Qa.Write("<un,Qa>");
 
   file_out->Close();
   return 0;
